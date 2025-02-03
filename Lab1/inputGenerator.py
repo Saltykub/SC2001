6a0922eafb357cc829f957368de2533b0735df03
keechongwei <keechongwei@gmail.com>
2025-02-03 16:47:57 +0800

    did some testing

--- Lab1/inputGenerator.py
+++ Lab1/inputGenerator.py
@@ -1,7 +1,7 @@
 import random
 
-n = 1000000 # magnitude of samples
-for i in range(1,11): 
+n = 10 # magnitude of samples
+for i in range(1,2): 
     f = open(f"input{str(i*n)}.txt","a")
     data = random.sample(range(1,10000000+1),i*n) # range of values from 1 to maximum allowed size of datasets
     for i in range(0,len(data)):
--- Lab1/main.py
+++ Lab1/main.py
@@ -5,7 +5,7 @@ from time import process_time
 # parameter for merge_sort is array, start index, stop index, step for using insertion sort 
 # merge_sort_hybrid(sample_data,0,4,1)
 #print(sample_data)
-f = open('Lab1/inputs/input10000000.txt',"r") #change path string depending on root folder
+f = open('Lab1/inputs/input1000.txt',"r") #change path string depending on root folder
 data = f.readline()
 data = data.split(",")
 for i in range(0,len(data)):
@@ -14,12 +14,13 @@ for i in range(0,len(data)):
 # Start the stopwatch / counter 
 t1_start = process_time() 
 
-data, keycomparisons = mergeSortwithComparisons(data)
+data,keycomparisons = mergeSortwithComparisons(data)
 # Stop the stopwatch / counter
 t1_stop = process_time()
 
 # key comps with Nathan's function: 220099687
 # key comps with Chong Wei's function: 220102334
+
 print(f"Key Comparisons:{keycomparisons}")
 print("Elapsed time during the whole program in seconds:",
                                          t1_stop-t1_start) 
