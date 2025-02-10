6810498ce048276ee0faf03fe32f0d762c89ed3d
Saltykub <nathan.juir@gmail.com>
2025-02-10 20:02:09 +0800

    refactor code

--- Lab1/inputGenerator.py
+++ Lab1/inputGenerator.py
@@ -1,13 +1,6 @@
 import random
 
-n = 10 # magnitude of samples
-for i in range(1,2): 
-    f = open(f"input{str(i*n)}.txt","a")
-    data = random.sample(range(1,10000000+1),i*n) # range of values from 1 to maximum allowed size of datasets
-    for i in range(0,len(data)):
-        if (i != len(data)-1):
-            f.write(str(data[i]) + ",")
-        else:
-            f.write(str(data[i]))
-f.close()
+def generate_sample(n):
+    data = [random.randint(1, 1000000) for _ in range(n)]  # range of values from 1 to maximum allowed size of datasets
+    return data
 
--- Lab1/utils.py
+++ Lab1/utils.py
@@ -1,10 +1,12 @@
 def insertion_sort (arr,l,r):
     keys = 0
-    for i in range (l,r+1):
+    for i in range (l+1,r+1):
         for j in range(i,l,-1):
+            keys+=1
             if(arr[j] < arr[j-1]):
-                keys+=1
                 arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
     return keys
 
 def merge(arr,l,r):
@@ -14,13 +16,12 @@ def merge(arr,l,r):
     j = mid+1
     temp = []
     while(i <= mid and j <= r):
+        keys+=1
         if(arr[i] < arr[j]):
             temp.append(arr[i])
-            keys+=1
             i+=1
         else: 
             temp.append(arr[j])
-            keys+=1
             j+=1
     while(i <= mid):
         temp.append(arr[i])
@@ -50,42 +51,42 @@ def merge_sort_hybrid(arr,l,r,s):
         keys += insertion_sort(arr,l,r)
     else:
         mid = (l+r) // 2
-        keys += merge_sort_hybrid(arr,l,mid,s)
+        keys+= merge_sort_hybrid(arr,l,mid,s)
         keys+= merge_sort_hybrid(arr,mid+1,r,s)
         keys+= merge(arr,l,r)
     return keys
 
 
-def mergeSortwithComparisons(arr):
-    if len(arr) <= 1:
-        return arr,0
+# def mergeSortwithComparisons(arr):
+#     if len(arr) <= 1:
+#         return arr,0
 
-    mid = len(arr) // 2
-    leftHalf = arr[:mid]
-    rightHalf = arr[mid:]
-    totalkeycomparisons = 0
-    sortedLeft,leftkeycomparisons = mergeSortwithComparisons(leftHalf)
-    sortedRight,rightkeycomparisons = mergeSortwithComparisons(rightHalf)
-    totalkeycomparisons = leftkeycomparisons+rightkeycomparisons
-    return mergeWithComparisons(sortedLeft, sortedRight,totalkeycomparisons)
+#     mid = len(arr) // 2
+#     leftHalf = arr[:mid]
+#     rightHalf = arr[mid:]
+#     totalkeycomparisons = 0
+#     sortedLeft,leftkeycomparisons = mergeSortwithComparisons(leftHalf)
+#     sortedRight,rightkeycomparisons = mergeSortwithComparisons(rightHalf)
+#     totalkeycomparisons = leftkeycomparisons+rightkeycomparisons
+#     return mergeWithComparisons(sortedLeft, sortedRight,totalkeycomparisons)
 
-def mergeWithComparisons(left, right,totalkeycomparisons):
-    result = []
-    i = j = 0
+# def mergeWithComparisons(left, right,totalkeycomparisons):
+#     result = []
+#     i = j = 0
 
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-        totalkeycomparisons+=1
+#     while i < len(left) and j < len(right):
+#         if left[i] < right[j]:
+#             result.append(left[i])
+#             i += 1
+#         else:
+#             result.append(right[j])
+#             j += 1
+#         totalkeycomparisons+=1
 
-    result.extend(left[i:])
-    result.extend(right[j:])
+#     result.extend(left[i:])
+#     result.extend(right[j:])
 
-    return result,totalkeycomparisons
+#     return result,totalkeycomparisons
 
 
 
